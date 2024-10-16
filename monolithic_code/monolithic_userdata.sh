ef07784b83d93849dc6085ea093df078f1155689
kh3654po <kh3654po@gmail.com>
2024-10-16 22:53:34 +0900

    user data 코드 수정

--- monolithic_code/monolithic_userdata.sh
+++ monolithic_code/monolithic_userdata.sh
@@ -11,14 +11,18 @@ npm install
 
 # DB setup
 #######################################
-# DB_EP 수정할 것!!!
+# must modify!!!
 export DB_EP=!!!Must modify!!!
+export DB_MASTER_NAME="!!!Must modify!!!"
+export DB_PASSWD="!!!Must modify!!!"
 #######################################
 echo "export DB_EP=${DB_EP}" >> /home/ubuntu/.bashrc
-mysql -u admin -plab-password -h ${DB_EP} -P 3306 -e "CREATE USER 'nodeapp' IDENTIFIED WITH mysql_native_password BY 'coffee'";
-mysql -u admin -plab-password -h ${DB_EP} -P 3306 -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, PROCESS, REFERENCES, INDEX, ALTER, SHOW DATABASES, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER ON *.* TO 'nodeapp'@'%' WITH GRANT OPTION;"
-mysql -u admin -plab-password -h ${DB_EP} -P 3306 -e "CREATE DATABASE COFFEE;"
-mysql -u admin -plab-password -h ${DB_EP} -P 3306 -e "USE COFFEE; CREATE TABLE suppliers(id INT NOT NULL AUTO_INCREMENT,name VARCHAR(255) NOT NULL,address VARCHAR(255) NOT NULL,city VARCHAR(255) NOT NULL,state VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL,phone VARCHAR(100) NOT NULL,PRIMARY KEY ( id ));"
+echo "export DB_MASTER_NAME=${DB_MASTER_NAME}" >> /home/ubuntu/.bashrc
+echo "export DB_PASSWD=${DB_PASSWD}" >> /home/ubuntu/.bashrc
+mysql -u ${DB_MASTER_NAME} -p${DB_PASSWD} -h ${DB_EP} -P 3306 -e "CREATE USER 'nodeapp' IDENTIFIED WITH mysql_native_password BY 'coffee'";
+mysql -u ${DB_MASTER_NAME} -p${DB_PASSWD} -h ${DB_EP} -P 3306 -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, PROCESS, REFERENCES, INDEX, ALTER, SHOW DATABASES, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER ON *.* TO 'nodeapp'@'%' WITH GRANT OPTION;"
+mysql -u ${DB_MASTER_NAME} -p${DB_PASSWD} -h ${DB_EP} -P 3306 -e "CREATE DATABASE COFFEE;"
+mysql -u ${DB_MASTER_NAME} -p${DB_PASSWD} -h ${DB_EP} -P 3306 -e "USE COFFEE; CREATE TABLE suppliers(id INT NOT NULL AUTO_INCREMENT,name VARCHAR(255) NOT NULL,address VARCHAR(255) NOT NULL,city VARCHAR(255) NOT NULL,state VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL,phone VARCHAR(100) NOT NULL,PRIMARY KEY ( id ));"
 
 # sed the config file
 sed -i "s|REPLACE-DB-HOST|${DB_EP}|g" /home/ubuntu/cs-cloud-2024/monolithic_code/app/config/config.js
